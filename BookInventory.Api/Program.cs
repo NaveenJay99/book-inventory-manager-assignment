59dbd18a5b1763a3933442e1f7fbd64c17a5cd96
Naveen Jayathissa <naveenjayathis@gmail.com>
2026-08-20 16:46:55 +0530

    configure controllers

--- BookInventory.Api/Program.cs
+++ BookInventory.Api/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
