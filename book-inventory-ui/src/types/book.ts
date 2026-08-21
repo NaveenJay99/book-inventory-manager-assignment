25c7d10893a3ff275e76dc5cde781f266336e7dc
Naveen Jayathissa <naveenjayathis@gmail.com>
2026-08-21 12:33:51 +0530

    create book.ts

---
+++ book-inventory-ui/src/types/book.ts
@@ -0,0 +1,16 @@
+export interface Book {
+    id: number;
+    title: string;
+    author: string;
+    isbn?: string;
+    publishedYear: number;
+    price: number;
+    isAvailable: boolean;
+    description?: string;
+    coverImageUrl?: string;
+    pageCount?: number;
+    language?: string;
+}
+
+export type BookFormValues = Omit<Book,
+'id'>;
