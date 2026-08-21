5e9d216c124b9a676c43f4c61414995d1a166d62
Naveen Jayathissa <naveenjayathis@gmail.com>
2026-08-21 11:21:03 +0530

    update BookDetail model and Program configuration

--- BookInventory.Api/Program.cs
+++ BookInventory.Api/Program.cs
@@ -5,7 +5,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler =
+            System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    });
+
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
